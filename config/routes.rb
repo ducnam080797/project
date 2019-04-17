788f791035f4d23ea545a2d53f6b94ea7cf84de3
Duc Nam <ducnam0807@gmail.com>
2019-04-17 11:51:25 +0700

    layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root "static_pages#home"
+  get "static_pages/help"
+  # delete "/logout", to: "sessions#destroy"
+  # resources :microposts, only: %i(create destroy)
+  # resources :relationships, only: %i(create destroy)
 end
--- db/migrate/20190409064746_create_users.rb
+++ db/migrate/20190409064746_create_users.rb
@@ -2,10 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
-      t.string :image
       t.datetime :birthday
-      t.boolean :sex
+      t.string :sex
 
       t.timestamps
     end
